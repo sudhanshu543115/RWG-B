5a051005369aac4de4b32a632391ec37933c453d
Neetesh Kumar Yadav <neeteshyadav0206@gmail.com>
2026-04-15 15:53:26 +0530

    Initial

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const PORT = process.env.PORT || 5000;
+
+dotenv.config();
+
+app.use(cors());
+app.use(express.json());
+
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
