c6c36468ba595844a737f691f37f2aeaecafb5b0
SUDHANSHU DUBEY <manudubey249@gmail.com>
2026-04-18 09:54:19 +0530

    undefined

--- models/tourist/User.js
+++ models/tourist/User.js
@@ -4,6 +4,7 @@ const userSchema = new mongoose.Schema({
     phone: String,
     name: String,
     profileCompleted: { type: Boolean, default: false }
+    
 });
 
 export default mongoose.models.User || mongoose.model("User", userSchema);
