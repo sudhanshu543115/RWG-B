e80342c6bcf32cc08122240f119c7d4ab0b3e0e8
SUDHANSHU DUBEY <manudubey249@gmail.com>
2026-04-24 10:10:39 +0530

    updated

--- config/razorpay.js
+++ config/razorpay.js
@@ -1,8 +1,8 @@
-// import Razorpay from "razorpay";
+import Razorpay from "razorpay";
 
-// const razorpay = new Razorpay({
-//   key_id: process.env.TEST_API_KEY,
-//   key_secret: process.env.TEST_API_SECRET
-// });
+const razorpay = new Razorpay({
+  key_id: process.env.TEST_API_KEY,
+  key_secret: process.env.TEST_API_SECRET
+});
 
-// export default razorpay;
+export default razorpay;
